5420c4abc0c4a5b1de77ba1ce05b63159342f637
xxc <778994556@qq.com>
2020-11-03 11:11:19 +0800

    update activityService

--- src/service/ActivityService.ts
+++ src/service/ActivityService.ts
@@ -1,7 +1,6 @@
-import ActivityDao from "../dao/ActivityDao";
-import BaseService from "../../base/service/abstract/BaseService";
 import TopService from "../../base/service/TopService";
 import App from "../../base/App";
+import BaseActivityService from "../../base/service/ActivityService";
 
 /**
  *
@@ -17,85 +16,12 @@ type activityData = {
     data: any
 }
 
-export default class ActivityService extends BaseService<ActivityDao<any>, any> {
+export default class ActivityService extends BaseActivityService {
     constructor(app: App) {
-        super(ActivityDao, app);
+        super(app);
     }
-
-    private activity: any;
 
     private topService = this.getService(TopService)
-
-    /**
-     * 获取活动状态
-     */
-    async getActivityStatus(): Promise<number> {
-        let activity;
-        //如果当前活动存在
-        if (this.activity) {
-            activity = this.activity.data;
-        }
-        //否则查询活动
-        else {
-            let filter: any = {};
-            !this.activityId || (filter._id = this.activityId);
-            activity = await super.get(filter, {
-                projection: {
-                    _id: 0,
-                    startTime: 1,
-                    endTime: 1
-                }
-            });
-        }
-        return this.status(activity);
-    }
-
-    /**
-     * 获取活动
-     */
-    async getActivity(
-        //获取活动字段
-        projection?: any
-    ): Promise<activityData> {
-        //如果目标活动已经被实例化
-        if (this.activity && this.activity.code !== -1 && this.activity.data._id === this.activityId) {
-            return this.activity;
-        } else {
-            //返回值
-            let result: any = {};
-            //过滤参数
-            let filter: any = {};
-            !this.activityId || (filter._id = this.activityId)
-            let options: any = {};
-            !projection || (options.projection = projection)
-            //查询活动
-            let activity = await super.get(filter, options);
-            result.code = this.status(activity);
-            //带上活动返回
-            result.data = activity;
-            this.activity = result;
-            return this.activity;
-        }
-    }
-
-    private status(activity: any): number {
-        //没有活动
-        if (!activity) {
-            return -1;
-        }
-        //活动未开始
-        if (this.time().common.base < activity.startTime) {
-            return 0;
-        }
-        //活动已结束
-        else if (this.time().common.base > activity.endTime) {
-            return 2
-        }
-        //活动正常
-        else {
-            return 1;
-        }
-    }
 
     async grant() {
         let prizes = [];
