6911cc3d7e1f7b91552eedc313020fbbdea944e3
xxc <778994556@qq.com>
2021-06-15 18:00:08 +0800

    update

--- index.ts
+++ index.ts
@@ -94,19 +94,6 @@ export async function editAppConfig(context) {
     return await gmtaobao.activity.editAppConfig(context);
 }
 
-
-const modules = [ActivityService];
-for (let entry of Object.entries(XApp.exports)) {
-    // @ts-ignore
-    exports[entry[0]] = async (context) => {
-        const app = new App(context, entry[0]);
-        app.runNeedParams = entry[1].params || {};
-        if (!entry[1].needGlobalParam) {
-            app.globalNeedParams = {};
-        }
-        entry[1].before.forEach(v => v.call(app.before))
-        return await app.run(async function () {
-            await app.getService(entry[1].constructor)[entry[0]]();
-        });
-    }
-}
+[ActivityService];
+// @ts-ignore
+exports = XApp.initExpose(App);
